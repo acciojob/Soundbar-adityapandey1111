98b6a612e8dddfb5ece6ac12ca5b60b4a5f3f15d
Aditya Kumar Pandey <114282369+adityapandey1111@users.noreply.github.com>
2023-06-17 20:46:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+    const buttons = document.querySelectorAll('.btn');
+    const stopButton = document.querySelector('.stop');
+    const audioFolder = './sounds/';
+  
+    buttons.forEach(function(button) {
+      button.addEventListener('click', function() {
+        const soundName = button.innerText;
+        const audio = new Audio(audioFolder + soundName + '.mp3');
+        audio.play();
+      });
+    });
+  
+    stopButton.addEventListener('click', function() {
+      const audioElements = document.getElementsByTagName('audio');
+      for (let i = 0; i < audioElements.length; i++) {
+        audioElements[i].pause();
+        audioElements[i].currentTime = 0;
+      }
+    });
+  });
+  
